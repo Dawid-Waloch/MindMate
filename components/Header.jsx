5316430714894b3aa23db34918d9f2d7b3164def
Dawid <dawwal1900@gmail.com>
2025-09-20 00:34:38 +0200

    header fixed, link problem

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,11 +7,9 @@ const Header = () => {
 
     return (
         <div className="flex w-full h-20 max-w-3xl items-center justify-center bg-white/20 rounded-lg shadow-lg ">
-            <Link className="flex-1" href={'/'}>
-                <div className="pl-5 text-2xl">
-                    MindMate
-                </div>
-            </Link>
+            <div className="flex-1 pl-5 text-2xl">
+                <Link href={'/'}>MindMate</Link>
+            </div>
             <div className="flex pr-5 text-xl gap-3 items-center">
                 <Link href={'/chat'}>
                     <div className="bg-white/20 rounded-lg p-2">💬 Chat</div>
