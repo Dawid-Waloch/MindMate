416814dfe201890d942cc4c122d965b1640fc02f
Dawid <dawwal1900@gmail.com>
2025-09-26 16:33:39 +0200

    Breat exercise fixed

--- pages/breath/index.jsx
+++ pages/breath/index.jsx
@@ -4,31 +4,40 @@ import Header from "../../components/Header";
 const BreathPage = () => {
     const [currentStep, setCurrentStep] = useState(0);
     const [exerciseOn, setExercieOn] = useState(false);
+    const [showInitialStep, setShowInitialStep] = useState(true);
     const timeoutRef = useRef(null);
-
-    const steps = [
+    const initialSteps = useRef([
+        { text: "Prepare yourself to exercise", scale: 0, duration: 4000 },
         { text: "Inhale through nose", scale: 1, duration: 4000 },
         { text: "Hold", scale: 1, duration: 7000 },
         { text: "Exhale", scale: 0, duration: 8000 },
         { text: "Hold", scale: 0, duration: 2000 },
-    ];
+    ]);
+    const [steps, setSteps] = useState(initialSteps.current);
+    const displaySteps = showInitialStep ? steps : steps.slice(1);
 
     useEffect(() => {
         if(!exerciseOn) {
             clearInterval(timeoutRef.current);
             setCurrentStep(0);
+            setSteps(initialSteps.current);
+            setShowInitialStep(true);
             return;
         }
 
-        const { duration } = steps[currentStep];
+        const { duration } = displaySteps[currentStep];
         timeoutRef.current = setInterval(() => {
-            setCurrentStep((prev) => (prev + 1) % steps.length);
+            if(showInitialStep) {
+                setShowInitialStep(false);
+            } else {
+                setCurrentStep((prev) => (prev + 1) % displaySteps.length);
+            }
         }, duration)
 
-        return () => clearTimeout(timeoutRef.current);
-    }, [exerciseOn, currentStep])
+        return () => clearInterval(timeoutRef.current);
+    }, [exerciseOn, currentStep, steps, showInitialStep])
 
-    const { text, scale, duration } = steps[currentStep];
+    const { text, scale, duration } = displaySteps[currentStep];
 
     return (
         <div className="relative min-h-screen bg-gradient-to-br from-teal-400 via-blue-500 to-purple-600 text-white">
@@ -36,7 +45,7 @@ const BreathPage = () => {
                 <Header />
             </div>
             <div className="flex flex-col items-center min-h-screen">
-                <div className="mt-3 text-2xl font-bold">{exerciseOn ? text : "Click Start Execrice"}</div>
+                <div className="mt-3 text-2xl font-bold">{exerciseOn ? text : "Click Start Exercise"}</div>
                 <div className="mt-10 rounded-full bg-white/20 w-80 h-80">
                     <div
                         className={`rounded-full bg-blue-500 transition-transform ease-in-out w-80 h-80`}
